31a3d539c25a434344f35a4f7e63ec1f07629954
ChristopherCarlton <89569738+ChristopherCarlton@users.noreply.github.com>
2024-01-06 23:17:27 -0800

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,6 +9,11 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <button className="bg-[#1E40AF] text-white py-3 px-8 rounded-full mr-4">
+                    <Link href="/AboutMe">
+                        RESUME
+                    </Link>
+                </button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
