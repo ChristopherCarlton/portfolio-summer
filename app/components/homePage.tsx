d419dfc7f8874170b17852f5aef7089c8fc0aaad
ChristopherCarlton <89569738+ChristopherCarlton@users.noreply.github.com>
2024-01-06 23:14:09 -0800

    Add files via upload

---
+++ app/components/homePage.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#d29d61]">
+      <header className="flex justify-between items-center py-4 px-8 bg-[#ffffff] shadow-md">
+        <div className="text-[#1E40AF] font-semibold text-xl">Summer Vu</div>
+        <nav>
+          <ul className="flex space-x-6 font-medium">
+            <li className="hover:text-[#1E40AF] text-black transition-colors duration-300">
+              ABOUT ME
+            </li>
+            <li className="hover:text-[#1E40AF] text-black transition-colors duration-300">
+                <Link href="/../Resume">
+                    RESUME
+                </Link>
+            </li>
+            <li className="hover:text-[#1E40AF] text-black transition-colors duration-300">
+              CLASSES
+            </li>
+            <li className="hover:text-[#1E40AF] text-black transition-colors duration-300">
+              PROJECTS
+            </li>
+            <li className="hover:text-[#1E40AF] text-black transition-colors duration-300">
+              CONTACT
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="flex flex-1">
+        <div className="w-full lg:w-1/3 xl:w-1/4 p-12 flex flex-col items-center bg-[#3d83a4]">
+          <img
+            src="/images/summer.png"
+            alt="Dani Berch portrait"
+            className="w-48 h-48 rounded-full shadow-lg"
+          />
+          <h3 className="font-semibold text-2xl mt-6">Summer Vu</h3>
+          <p className="font-light text-lg">Strategic Communicator</p>
+          <hr className="border-t border-[#1E40AF] w-16 mt-2" />
+          <div className="flex justify-center space-x-4 mt-6">
+            <a href="#" className="text-[#1E40AF] hover:text-[#93C5FD]">
+              <i className="fa fa-linkedin fa-lg"></i>
+            </a>
+            <a href="#" className="text-[#1E40AF] hover:text-[#93C5FD]">
+              <i className="fa fa-facebook fa-lg"></i>
+            </a>
+            <a href="#" className="text-[#1E40AF] hover:text-[#93C5FD]">
+              <i className="fa fa-instagram fa-lg"></i>
+            </a>
+          </div>
+        </div>
+        <div className="w-full lg:w-2/3 xl:w-3/4 p-12 bg-[#F3F4F6] mx-auto">
+            <h1 className="flex justify-center font-semibold text-7xl text-black mb-6">Hello</h1>
+            <h2 className="flex justify-center font-light text-3xl text-black mb-6">
+                Here's who I am & what I do
+            </h2>
+            <div className="flex justify-center">
+                <button className="bg-[#1E40AF] text-white py-3 px-8 rounded-full mr-4">
+                    <Link href="/../Resume">
+                        RESUME
+                    </Link>
+                </button>
+                <button className="border border-[#1E40AF] text-[#1E40AF] py-3 px-8 rounded-full">
+                    PROJECTS
+                </button>
+            </div>
+            <p className="mt-6 text-lg text-black font-light px-32"> 
+                With nearly three years in the communication industry and a degree
+                in Strategic and Corporate Communication, I am excited to bring the
+                skills I have acquired to an organization's culture. I pride myself
+                on always taking on tasks with an eagerness to learn, the confidence
+                to succeed, and the humility to ask questions and ask for guidance
+                along the way.
+            </p>
+            <div className="flex flex-col items-start px-32 mt-10"> {/* Aligns items vertically and to the start (left side) */}
+                <div className="flex items-center mb-4"> {/* Each item group */}
+                    <p className="text-[#374151] uppercase tracking-widest text-xs mr-4">Call</p> {/* Added margin for separation */}
+                    <p className="text-[#374151]">619-600-9797</p>
+                </div>
+                <div className="flex items-center"> {/* Each item group */}
+                    <p className="text-[#374151] uppercase tracking-widest text-xs mr-4">Write</p> {/* Added margin for separation */}
+                    <p className="text-[#374151]">summerv2001@yahoo.com</p>
+                </div>
+            </div> 
+        </div>
+      </div>
+    </div>
+  );
+}
